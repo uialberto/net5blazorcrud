c91ef84eddda758d407da9a5eb2385891138abb5
Alberto Baigorria <uialberto@gmail.com>
2021-03-05 00:13:31 -0400

    Creamos un CRUD en .NET 5 (3/4)

---
+++ CrudNet5Blazor/CrudNet5Blazor/Controllers/CursosController.cs
@@ -0,0 +1,41 @@
+﻿using CrudNet5Blazor.DataAccess;
+using CrudNet5Blazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNet5Blazor.Controllers
+{
+    public class CursosController : Controller
+    {
+        private readonly AppContextDb _context;
+
+        public CursosController(AppContextDb context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Curso> list = _context.Cursos;
+            return View(list);
+        }
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Crear(Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(curso);
+                _context.SaveChanges();
+            }
+
+            return View();
+        }
+    }
+}
